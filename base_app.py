624991cf65db54043400f2d7157b0e87d0011c24
AmosMaps <amosphashe@gmail.com>
2024-06-27 19:05:07 +0200

    Updated

--- base_app.py
+++ base_app.py
@@ -65,46 +65,43 @@ def main():
 		st.markdown("Some information here")
 
 		
-	# Building out the predication page
-	if selection == "Prediction":
-		st.info("Prediction with ML Models")
-		# Creating a text box for user input
-		news_text = st.text_area("Enter Text","Type Here")
+# Building out the prediction page
+if selection == "Prediction":
+    st.info("Prediction with ML Models")
+    # Creating a text box for user input
+    news_text = st.text_area("Enter Text", "Type Here")
+    
+    model_name = ["Logistic Regression Classifier", "Support Vector Classifier", "Random Forest Classifier"]
+    # selection = st.sidebar.selectbox("Choose Option", options)
+    model_choice = st.selectbox("Select a Classifier Model", model_name)
+
+    if st.button("Classify"):
+        # Transforming user input with vectorizer
+        vect_text = test_cv.transform([news_text]).toarray()
         
-        model_name = ["Logistic Regression Classifier", "Support Vector Classifier", "Random Forest Classifier"]
-		#selection = st.sidebar.selectbox("Choose Option", options)
-		model_choice = st.selectbox(
-                "Select a Classifier  Model", model_name)
+        if model_choice == "Logistic Regression Classifier":
+            # Load your .pkl file with the model of your choice + make predictions
+            # Try loading in multiple models to give the user a choice
+            lg = joblib.load(open(os.path.join("models.pkl"), "rb"))
+            prediction = lg.predict(vect_text)
+        elif model_choice == "Support Vector Classifier":
+            svc_classifier = joblib.load(open(os.path.join("models.pkl"), "rb"))
+            prediction = svc_classifier.predict(vect_text)
+        elif model_choice == "Random Forest Classifier":
+            rf = joblib.load(open(os.path.join("models.pkl"), "rb"))
+            prediction = rf.predict(vect_text)
 
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = test_cv.transform([news_text]).toarray()
-            
-            if model_choice == "Logistic Regression Classifier":
-				# Load your .pkl file with the model of your choice + make predictions
-				# Try loading in multiple models to give the user a choice
-				lg = joblib.load(open(os.path.join("models.pkl"),"rb"))
-				prediction = lg.predict(test_vect)
-			elif model_choice == "Support Vector Classifier":
-				svc_classifier = joblib.load(open(os.path.join("models.pkl"),"rb"))
-				prediction = svc_classifier.predict(test_vect)
-			elif model_choice == "Random Forest Classifier":
-				rf = joblib.load(open(os.path.join("models.pkl"),"rb"))
-				prediction = rf.predict(test_vect)
-
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-
-			if prediction == 1 :
-				st.success("Text Categorized as: {}".format(prediction))
-			elif prediction == 0 :
-				st.success("Text Categorized as: {}".format(prediction))
-			elif prediction == 3 :
-				st.success("Text Categorized as: {}".format(prediction))
-			elif prediction == 4 :
-				st.success("Text Categorized as: {}".format(prediction))
-
+        # When model has successfully run, will print prediction
+        # You can use a dictionary or similar structure to make this output
+        # more human interpretable.
+        if prediction == 1:
+            st.success("Text Categorized as: {}".format(prediction))
+        elif prediction == 0:
+            st.success("Text Categorized as: {}".format(prediction))
+        elif prediction == 3:
+            st.success("Text Categorized as: {}".format(prediction))
+        elif prediction == 4:
+            st.success("Text Categorized as: {}".format(prediction))
 
 			
 
